db9070f693b82278656846ef863d1b01cd89c34d
Frank Niesten <f.niesten@fontys.nl>
2018-03-29 12:32:20 +0200

    Bug Fixes

--- data/ui/register_warning_no_email.php
+++ data/ui/register_warning_no_email.php
@@ -1,3 +1,3 @@
 <div class="alert alert-warning" role="alert">
-Uw gebruikte inlog methode bevat geen email adres. Hierdoor is het niet mogelijk om een account aan te maken! Verifieer eerst uw email adres bij de betreffende Identity Provider en probeer daarna opnieuw een account aan te maken in de FPLO omgeving!</p> 
+Uw gebruikte inlog methode bevat geen email adres. Hierdoor is het niet mogelijk om een account aan te maken! Verifieer eerst uw email adres bij de betreffende Identity Provider en probeer het daarna opnieuw een account aan te maken in de FPLO omgeving!</p> 
 </div>
