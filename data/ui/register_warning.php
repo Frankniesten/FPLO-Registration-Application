1327c84fa98dbdcf56914a656475905a909aede7
Frank Niesten <f.niesten@fontys.nl>
2018-04-12 12:49:28 +0200

    text update

--- data/ui/register_warning.php
+++ data/ui/register_warning.php
@@ -1,3 +1,3 @@
 <div class="alert alert-warning" role="alert">
-	Het email adres is reeds geregistreerd in de FPLO omgeving. <a href="../registreren.php" class="alert-link">Klik hier</a> om een ander email adres te registreren!</p> 
+	Het e-mailadres is reeds geregistreerd in de SURF pilot modulaire leeromgeving. <a href="../registreren.php" class="alert-link">Registreer aan ander e-mailadres!</a></p> 
 </div>
--- data/ui/register_warning_no_email.php
+++ data/ui/register_warning_no_email.php
@@ -1,3 +1,3 @@
 <div class="alert alert-warning" role="alert">
-Uw gebruikte inlog methode bevat geen email adres. Hierdoor is het niet mogelijk om een account aan te maken! Verifieer eerst uw email adres bij de betreffende Identity Provider en probeer het daarna opnieuw een account aan te maken in de FPLO omgeving!</p> 
+Het e-mailadres is niet geverifieerd bij de gekozen Identity Provider. Verifieer eerst uw e-mailadres en probeer daarna opnieuw een account aan te maken in de SURF pilot modulaire leeromgeving!</p> 
 </div>
